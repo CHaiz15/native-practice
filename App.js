b0f67a0bd33d65994738c3c1a9c42ab05153be5b
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-04-02 18:04:12 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Test')
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('passed test')}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
